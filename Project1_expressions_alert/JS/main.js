0e1432027ad5345923ad5b2b4951b5dc3f34a289
Josephbacona <85919565+Josephbacona@users.noreply.github.com>
2021-08-18 15:08:13 -0700

    Added files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+
+var A = "This is a string!";              //Assigning a string to a variable
+document.write(A);                        //This is writing the var A
+window.alert(A)                           //This creates a window popup
+document.write("Bob told Bill, \"You Better stop or I'm telling mom!\" \"Eat some food!\" Bill responded.");          //Used "escaping" with
+var B = "Concatenated" + " String"                             //Assigning a concatenated string value                //the backslash.
+document.write(B);                                             //to a variable.
+var C = "These variables are";
+var D = " concatenated now."
+document.write(C + D);                                          //Concatenating two variables.
+var Family = "The Smiths", Dad = "James", Mom = "Nancy",          //Assigning multiple variables
+    Daughter = "Lucy", Son = "Jimmy";                             //in one statement.
+
+var Dad = Dad.fontcolor("blue");                                //Changed the font color with
+var Mom = Mom.fontcolor("green");                               //the str.fontcolor method
+var Daughter = Daughter.fontcolor("purple");
+var Son = Son.fontcolor("orange");
+document.write(Dad);
+document.write(4 + 4);                                        //Made an expression.
